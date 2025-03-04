df2b984bca4945c3439ab58b4eee21ac7e7e0e57
haipesintetic <81931461+haipesintetic@users.noreply.github.com>
2025-03-04 08:47:49 +0200

    Gradle lab 2

---
+++ module/src/models/Arbiter.java
@@ -0,0 +1,8 @@
+package models;
+
+public class Arbiter extends User
+{
+    public Arbiter(int id, String password, Role role) {
+        super(id, password, role);
+    }
+}
---
+++ module/src/models/User.java
@@ -0,0 +1,16 @@
+package models;
+
+public class User
+{
+    private int id;
+    private String password;
+    private Role role;
+
+    public User(int id, String password, Role role)
+    {
+        this.id = id;
+        this.password = password;
+        this.role = role;
+    }
+
+}
